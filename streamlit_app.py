23a218ec2024922ceafe6034aedab126b6db8cb2
Jordan Felan <80652743+Jordan-felan@users.noreply.github.com>
2022-04-15 14:19:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.header(' Breakfast Favorites')
+streamlit.text(':stew: Omega 3 & Blueberry Oatmeal')
 streamlit.text('OKale, Spinach, & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
