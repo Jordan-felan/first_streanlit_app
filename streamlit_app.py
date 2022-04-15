cd56d008304755844c8f09f3edb5f0f24a90065a
Jordan Felan <80652743+Jordan-felan@users.noreply.github.com>
2022-04-15 14:21:57 -0500

    Update streamlit_app.py
    added emojis and modified menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title("My Parents New Healthy Diner")
 
 streamlit.header(' Breakfast Favorites')
-streamlit.text(':stew: Omega 3 & Blueberry Oatmeal')
-streamlit.text('OKale, Spinach, & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🍲 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🍹 Kale, Spinach, & Rocket Smoothie')
+streamlit.text('🥚 Hard-Boiled Free-Range Egg')
+streamlit.text('🍞 Avocado Toast')
